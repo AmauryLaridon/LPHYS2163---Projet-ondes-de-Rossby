7cf1d79f96ad5a992a50cd5876c230a8c44033bc
Amaury Laridon <amaurylaridon@protonmail.com>
2022-12-14 18:04:52 +0100

    impl. résolution psi(zeta) Gus + début implt. animation affichage.

--- sim.py
+++ sim.py
@@ -1,12 +1,13 @@
-import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 
 
 ###################################################### Paramètres de la simulation ##################################################
 
 phi_0 = ((2*np.pi)/360)*45  # latitude en radian
 Delta_s = 200000  # résolution de la maille spatiale en mètre
-Delta_t = 3600  # résolution de la maille temporelle en seconde
+Delta_t = 360  # résolution de la maille temporelle en seconde
 nbr_jour = 1  # nombre de jours de simulation
 T = 86400*nbr_jour  # temps total de simulation en seconde
 L_x = 12000000  # longueur du domaine selon x en mètre
@@ -103,7 +104,7 @@ def velocity_field(psi):
 
 
 def vort_flux(u, v, zeta):
-    """Donne la valeur du flux de la composante verticale de la vorticité relative. Prend comme argument le champ de vitesse.
+    """Donne la valeur du flux de la composante verticale de la vorticité relative F. Prend comme argument le champ de vitesse.
     Prend comme argument la matrice de la composante selon x du champ de vitesse, puis la matrice de la composante y du champs de vitesse
     , la matrice de la composante verticale de la vorticité relative."""
     F = np.zeros((N, M))
@@ -124,49 +125,86 @@ def vort_flux(u, v, zeta):
 
 
 """
-def poisson():
-
-    def B(M):
-
-    C = (-4)*np.eye(M)+np.diag(np.ones(M-1), 1)+np.diag(np.ones(M-1), -1)
-
-    BB = np.kron(np.eye(M), C)+np.kron(np.diag(np.ones(M-1), 1), np.eye(M)) + \
-        np.kron(np.diag(np.ones(M-1), -1), np.eye(M))
-    return BB
-
-    U = np.zeros((M+2, M+2))
-    UU = np.linalg.solve(B(M), F)
-    U[1:-1, 1:-1] = UU.reshape(M, M)
-
-    return []
+	Ici on définit une grande matrice A qui est utile pour la fonction psi(zeta) utilisée juste après. On la définit ici car
+	la fonction sera utilisée de nombreuse fois et recrée cette matrice et l'inverser à chaque fois prendrait beaucoup de temps
+	de calcul pour rien. Notez que les commentaires ajoutés au cours de la création de la matrice sont à comprendre
+	dans le contexte de la fonction psi(zeta) ci-dessous
 """
+# Définition d'une grande matrice carrée diagonale A de côté N x M avec -4 sur sa diagonale.
+A = -4*np.eye(N*M)
+"""
+On veut sur chaque ligne ajouter un terme "1" correspondant à l'emplacement des 4 valeurs adjacentes de psi_i,j. On a donc
+	- Deux 1 à gauche et à droite de la diagonale, respectivement pour psi_i,j+1 et psi_i,j-1
+	- Deux 1 à une distance M de la diagonale pour psi_i+1,j et psi_i-1,j
+
+"""
+for i in range(N*M):
+    A[i, (i+1) % (N*M)] = 1
+    A[i, i-1] = 1
+    A[i, (i+M) % (N*M)] = 1
+    A[i, i-M] = 1
+# print(A)
+A_inv = np.linalg.inv(A)
+# print(A_inv)
+
+
+def psi(zeta):
+    """Fonction qui permet de calculer la fonction de courant psi à partir de la vorticité relative zeta en inversant l'opérateur du laplacien.
+    On transforme par différence finie l'opération du laplacien en une matrice A qui donne un ensemble de NxM équations algébriques à NxM inconnes qu'on résoud
+    en inversant A."""
+    zeta_col = np.zeros((N*M, 1)
+                        )  # On met la matrice zeta en forme de colonne en parcourant de gauche à droite et de bas en haut.
+
+    for i in range(N):
+        for l in range(M):
+            zeta_col[l+i*M, 0] = zeta[i, l]
+
+    """
+	Ici intervient notre matrice A définie plus haut.
+
+	En définissant psi_col par rapport au tableau psi de la même manière que l'on a définit zeta_col par rapprot à zeta
+	notre système est donc donné par A psi_col = delta_s^2 zeta_col et sa solution est trouvée en inversant la matrice A
+	psi_col = A_inv delta_s^2 zeta_col.
+	"""
+    psi_col = (Delta_s)**2 * np.dot(A_inv, zeta_col)
+
+    # Pour finir il faut remettre psi en forme de tableau pour que ce soit cohérent avec le reste de l'implémentation.
+    psi = np.zeros((N, M))
+    for i in range(N):
+        for l in range(M):
+            psi[i, l] = psi_col[l+i*M, 0]
+
+    return psi
+
+
 ###################################################### Intégration temporelle ############################################################
 
-"""
+
 def vort_dynamic():
+    # On crée des tableaux à trois dimensions pour les deux dimensions spatiales et une dimension temporelle.
     stream_func_dyn = np.zeros((N, M, K))
     U = np.zeros((N, M, K))
     V = np.zeros((N, M, K))
     vort_dyn = np.zeros((N, M, K))
     F_dyn = np.zeros((N, M, K))
-    for i in range(K):
-        if i == 0:  # On pose les conditions initiale à t=0
+    for t in range(K-1):
+        if t == 0:  # On pose les conditions initiale à t=0
             stream_func_dyn[:, :, 0] = psi_init()
             U[:, :, 0] = velocity_field(psi_init())[0]
             V[:, :, 0] = velocity_field(psi_init())[1]
             vort_dyn[:, :, 0] = vort_init()
             F_dyn[:, :, 0] = vort_flux(U[:, :, 0], V[:, :, 0], vort_dyn[:, :, 0])
+
         else:  # On résoud dans le temps avec le schéma d'Euler avant.
-            stream_func_dyn[:, :, i] = poisson()
-            U[:, :, i] = velocity_field(stream_func_dyn[:, :, i])[0]
-            V[:, :, i] = velocity_field(stream_func_dyn[:, :, i])[1]
-            F_dyn[:, :, i] = vort_flux(U[:, :, i], V[:, :, i], vort_dyn[:, :, i])
-            vort_dyn[:, :, i+1] = F_dyn[:, :, i]*Delta_t + vort_dyn[:, :, i]
-    return vort_dyn
+            stream_func_dyn[:, :, t] = psi(vort_dyn[:, :, -1])
+            U[:, :, t] = velocity_field(stream_func_dyn[:, :, t])[0]
+            V[:, :, t] = velocity_field(stream_func_dyn[:, :, t])[1]
+            F_dyn[:, :, t] = vort_flux(U[:, :, t], V[:, :, t], vort_dyn[:, :, t])
+            vort_dyn[:, :, t+1] = F_dyn[:, :, t]*Delta_t + vort_dyn[:, :, t]
+            # print(vort_dyn)
+    return vort_dyn, U, V, stream_func_dyn
 
 
-solution = vort_dynamic()
-"""
 ###################################################### Résultats et figures ############################################################
 # Output de la résolution des mailles
 print("-----------------------------------------------------------------------------------------------")
@@ -176,7 +214,7 @@ print("Résolution numérique avec une maille temporelle de {} points".format(K)
 print("-----------------------------------------------------------------------------------------------")
 
 # Contourplot de la fonction de courant initiale
-
+"""
 X, Y = np.meshgrid(x, y)
 plt.contourf(X, Y, psi_init(), 100)
 plt.colorbar()
@@ -186,7 +224,7 @@ plt.legend(loc='best', shadow=True, fontsize="large")
 plt.xlabel("$x$", fontsize=20)
 plt.ylabel("$y$", fontsize=20)
 plt.tight_layout()
-plt.show()
+# plt.show()
 
 # Contourplot de la composante verticale de la vorticité relative initiale
 plt.contourf(X, Y, vort_init(), 100)
@@ -197,7 +235,7 @@ plt.legend(loc='best', shadow=True, fontsize="large")
 plt.xlabel("$x$", fontsize=20)
 plt.ylabel("$y$", fontsize=20)
 plt.tight_layout()
-plt.show()
+# plt.show()
 
 
 # Plot du champ de vitesse initial
@@ -219,4 +257,85 @@ plt.legend(loc='best', shadow=True, fontsize="large")
 plt.xlabel("$x$", fontsize=20)
 plt.ylabel("$y$", fontsize=20)
 plt.tight_layout()
+# plt.show()
+"""
+# Subplot des intégrations temporelles
+solution = vort_dynamic()
+vort_dyn = solution[0]
+U = solution[1]
+V = solution[2]
+stream_func_dyn = solution[3]
+
+X, Y = np.meshgrid(x, y)
+
+
+for t in range(K):
+    fig = plt.figure(figsize=[16/1.3, 9/1.3])
+    ax_stream_func = plt.subplot2grid((2, 2), (0, 0))
+    ax_U = plt.subplot2grid((2, 2), (0, 1))
+    ax_V = plt.subplot2grid((2, 2), (1, 1))
+    ax_vort = plt.subplot2grid((2, 2), (1, 0))
+    print("itérations = ", t)
+    nbr_heures = (t*Delta_t)/60
+    nbr_jours = nbr_heures/24
+    plt.suptitle("Temps : t = {:.2f} jours".format(nbr_jours))
+    ax_stream_func.contourf(X, Y, stream_func_dyn[:, :, t], 100)
+    # fig.colorbar(pcm)
+    ax_stream_func.set_title("$\psi(x,y,t)$")
+    ax_U.contourf(X, Y, U[:, :, t], 100)
+    # fig[0, 1].colorbar()
+    ax_U.set_title("$U(x,y,t)$")
+    ax_V.contourf(X, Y, V[:, :, t], 100)
+    # axs[1, 1].colorbar()
+    ax_V.set_title("$V(x,y,t)$")
+    ax_vort.contourf(X, Y, vort_dyn[:, :, t], 100)
+    # axs[1, 0].colorbar()
+    ax_vort.set_title("$\zeta(x,y,t)$")
+
+    plt.show()
+
+"""
+fig = plt.figure(figsize=[16/1.3, 9/1.3])
+ax_stream_func = plt.subplot2grid((2, 2), (0, 0))
+ax_U = plt.subplot2grid((2, 2), (0, 1))
+ax_V = plt.subplot2grid((2, 2), (1, 1))
+ax_vort = plt.subplot2grid((2, 2), (1, 0))
+
+#fig, ax_stream_func = plt.subplot()
+
+
+def animate(iter):
+    ax_stream_func.clear()
+    ax_stream_func.contourf(X, Y, stream_func_dyn[:, :, iter])
+    return ax_stream_func
+
+
+ani = animation.FuncAnimation(fig, animate, frames=500, blit=True, interval=1000/24, repeat=True)
+
 plt.show()
+
+for t in range(K):
+    print("itérations = ", t)
+    plt.pause(0.01)
+    nbr_heures = (t*Delta_t)/60
+    plt.suptitle("Temps : t = {:.2f}h".format(nbr_heures))
+    axs[0, 0].contourf(X, Y, stream_func_dyn[:, :, t])
+    # fig.colorbar(pcm)
+    axs[0, 0].set_title("$\psi(x,y,t)$")
+    axs[0, 1].contourf(X, Y, U[:, :, t])
+    # fig[0, 1].colorbar()
+    axs[0, 1].set_title("$U(x,y,t)$")
+    axs[1, 1].contourf(X, Y, V[:, :, t])
+    # axs[1, 1].colorbar()
+    axs[1, 1].set_title("$V(x,y,t)$")
+    axs[1, 0].contourf(X, Y, vort_dyn[:, :, t])
+    # axs[1, 0].colorbar()
+    axs[1, 0].set_title("$\zeta(x,y,t)$")
+
+    for ax in axs.flat:
+        ax.set(xlabel='$x$', ylabel='$y$')
+    for ax in axs.flat:  # Cache label x et y pour les plots sur les côtés.
+        ax.label_outer()
+
+    plt.show()
+"""
